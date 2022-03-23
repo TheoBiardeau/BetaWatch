8cda9879bdaf57cc9edd4efa992313e95bc3b181
TheoBiardeau <theo.biardeau@etu.univ-poitiers.fr>
2022-03-23 11:11:39 +0100

    first push project

---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+
+void app_main(void)
+{
+
+}
